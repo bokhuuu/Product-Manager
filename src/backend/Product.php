1b726a2b4e05da1e79142d5f8f8e67cfb940116b
bokhuuu <zurabokhua@gmail.com>
2024-07-31 16:58:55 +0400

    Implement Product abstract class
    - Added an abstract class to serve as a base for different product types in the application
    - Defined properties with corresponding getter and setter methods
    - Included abstract methods for saving product data to the database, ensuring subclasses implement specific logic, and for outputting product details, allowing for custom display logic in subclasses.

---
+++ src/backend/Product.php
@@ -0,0 +1,50 @@
+<?php
+abstract class Product {
+    protected $sku;
+    protected $name;
+    protected $price;
+    protected $type;
+
+    public function __construct($sku, $name, $price, $type) {
+        $this->setSku($sku);
+        $this->setName($name);
+        $this->setPrice($price);
+        $this->setType($type);
+    }
+
+    abstract public function save(PDO $db);
+    abstract public function display();
+
+    public function getSku() {
+        return $this->sku;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setSku($sku) {
+        $this->sku = $sku;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+    }
+}
+?>
