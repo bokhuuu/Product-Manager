43042f2fcc155c6a172255a2c2533848ab71a90a
bokhuuu <zurabokhua@gmail.com>
2024-08-01 10:51:48 +0400

    Set up routing for the application with React Router
    - Integrated React Router for routing, imported and configured necessary components
    - Applied Bootstrap CSS for styling
    - Established route for displaying the ProductList component and for the AddProduct component.

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add-product" element={<AddProduct />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
